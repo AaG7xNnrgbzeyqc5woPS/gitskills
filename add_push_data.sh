c1b30d508122447731e34bd34efa10c187f4974f
john <john_help@protonmail.com>
2020-01-18 21:47:00 +0800

    add_push_data.sh

--- add_push_data.sh
+++ add_push_data.sh
@@ -4,14 +4,43 @@
 # 
 function addpushafile()
 {
-filename=$1
-git add ${filename}
-git commit -m "${filename}"
-git push
-return 0  # return ok!
+  filename=$1
+  git add ${filename}
+  git commit -m "${filename}"
+  git push
+  return 0  # return ok!
+}
+
+
+function pushfiles()
+{
+  basename=$1
+  #begin=$2
+  #end=$3
+
+  for file in $(seq $2 $3)
+  do
+
+    addpushafile ${basename}${file}
+  done
+}
+
+
+
+function PushAllFiles()
+{ 
+  # for file in $(ls)
+  for file in *
+  do
+    addpushafile ${file}
+  done
 }
 
 
 
 #test function addpushafile
-addpushafile $1
+#addpushafile $1   # test ok
+
+#test pushfiles sxwc aa ae
+#pushfiles sxwc 0 5
+PushAllFiles
